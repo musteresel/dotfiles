ecfc1235fa82d6fa1701fe603d466f229b3ffe37
Daniel Jour <Daniel.Oertwig@gmail.com>
2016-04-21 02:32:46 +0200

    emacs - helm, column-enforce-mode and some fixes

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -29,10 +29,46 @@
 ;;; Autocompletion
 (use-package company
   :ensure t
-  :defer t
   :config (global-company-mode))
+
 
 ;;; Auto resize windows to make life easier
 (use-package golden-ratio
   :ensure t
-  :config (golden-ratio-mode 1))
+  :config
+  (golden-ratio-mode 1))
+
+
+;;; Nobody likes wide text
+(use-package column-enforce-mode
+  :ensure t
+  :config
+  (add-hook 'prog-mode-hook 'column-enforce-mode)
+  (setq column-enforce-column 80))
+
+
+;;; Use helm to direct me
+(use-package helm
+  :ensure t
+  :config
+  (require 'helm-config)
+  (global-set-key (kbd "C-c h") 'helm-command-prefix)
+  (global-unset-key (kbd "C-x c"))
+  (global-set-key (kbd "M-x") 'helm-M-x)
+  (setq helm-M-x-fuzzy-match t)
+  (global-set-key (kbd "M-y") 'helm-show-kill-ring)
+  (global-set-key (kbd "C-x b") 'helm-mini)
+  (setq helm-buffers-fuzzy-matching t
+	helm-recentf-fuzzy-match t)
+  (global-set-key (kbd "C-x C-f") 'helm-find-files)
+  (global-set-key (kbd "C-c h o") 'helm-occur)
+  (global-set-key (kbd "C-h SPC") 'helm-all-mark-rings)
+  (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action)
+  (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action)
+  (define-key helm-map (kbd "C-z") 'helm-select-action)
+  (add-to-list 'helm-sources-using-default-as-input 'helm-source-man-pages)
+  (when (executable-find "curl")
+    (setq helm-google-suggest-use-curl-p t))
+  (helm-mode 1))
+
+
