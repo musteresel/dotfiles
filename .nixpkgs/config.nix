2456830ea6035e7291f687259a92520dd66c6e7e
Daniel Jour <Daniel.Oertwig@gmail.com>
2016-04-21 02:31:43 +0200

    Flash and PDF for chromium

--- .nixpkgs/config.nix
+++ .nixpkgs/config.nix
@@ -1,4 +1,10 @@
 {
+  allowUnfree = true;
+  chromium = {
+    enablePepperFlash = true;
+    enablePepperPDF = true;
+  };
+
   packageOverrides = pkgs_ : with pkgs_; {
     desktop = with pkgs; buildEnv {
       name = "desktop";
